4f2cb094288d46911aba40a98d0f4cc30ad4e29c
Welder100 <welder.dmae@gmail.com>
2025-02-27 11:35:53 -0300

    Atualizando todos os arquivos com passando para caminhos absolutos

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,26 @@
 const CACHE_NAME = 'cruzadas-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json', // Adicione o manifest.json aqui
-  '/jogo.html',
-  '/lista.html',
-  '/style.css',
-  '/script.js',
-  '/cruzadas.js',
-  '/img/borracha.png',
-  '/img/config.png',
-  '/img/config1.png',
-  '/img/home.png',
-  '/img/quad.png',
-  '/img/seta-para-baixo.png',
-  '/img/seta-para-cima.png',
-  '/img/seta-para-direita.png',
-  '/img/seta-para-esquerda.png',
-  '/img/visao.png',
-  '/img/icon-192x192.png', // Ícone 192x192
-  '/img/icon-512x512.png',
-  // Adicione aqui todos os arquivos que deseja armazenar em cache
+  '/silabasCruzadas/',                  // Página raiz do subdiretório
+  '/silabasCruzadas/index.html',
+  '/silabasCruzadas/manifest.json',     // Adicione o manifest.json aqui
+  '/silabasCruzadas/jogo.html',
+  '/silabasCruzadas/lista.html',
+  '/silabasCruzadas/style.css',
+  '/silabasCruzadas/script.js',
+  '/silabasCruzadas/cruzadas.js',
+  '/silabasCruzadas/img/borracha.png',
+  '/silabasCruzadas/img/config.png',
+  '/silabasCruzadas/img/config1.png',
+  '/silabasCruzadas/img/home.png',
+  '/silabasCruzadas/img/quad.png',
+  '/silabasCruzadas/img/seta-para-baixo.png',
+  '/silabasCruzadas/img/seta-para-cima.png',
+  '/silabasCruzadas/img/seta-para-direita.png',
+  '/silabasCruzadas/img/seta-para-esquerda.png',
+  '/silabasCruzadas/img/visao.png',
+  '/silabasCruzadas/img/icon-192x192.png',  // Ícone 192x192
+  '/silabasCruzadas/img/icon-512x512.png',  // Ícone 512x512
+  // Adicione aqui outros arquivos que deseja armazenar em cache
 ];
 
 self.addEventListener('install', (event) => {
